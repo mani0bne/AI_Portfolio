54839e01dae9b0a759767e53e3f4cc65ae339187
Manihas Netha N <119604785+mani0bne@users.noreply.github.com>
2026-09-01 18:04:53 +0530

    Update uploadController.js

--- backend/controller/uploadController.js
+++ backend/controller/uploadController.js
@@ -1,17 +1,13 @@
 import fs from "fs";
 import { extractTextFromPDF } from "../utils/pdfParser.js";
 import { generatePortfolioJSON } from "../utils/gemini.js";
-<<<<<<< HEAD
 import { generatePortfolioData } from "../utils/groqService.js";
-=======
->>>>>>> 439d731f31ec0cfc365a3cdf5195d641754da135
 
 export const uploadResume = async (req, res) => {
   try {
     const filePath = req.file.path;
 
-<<<<<<< HEAD
-    // ✅ Extract text
+
     const resumeText = await extractTextFromPDF(filePath);
 
     let portfolioData;
@@ -34,20 +30,19 @@ export const uploadResume = async (req, res) => {
 
     res.status(200).json(portfolioData);
 
-=======
     const resumeText = await extractTextFromPDF(filePath);
     const portfolioData = await generatePortfolioJSON(resumeText);
 
     fs.unlinkSync(filePath); // cleanup
 
     res.status(200).json(portfolioData);
->>>>>>> 439d731f31ec0cfc365a3cdf5195d641754da135
+
   } catch (error) {
     console.error("UPLOAD ERROR:", error);
     res.status(500).json({ error: "Failed to process resume" });
   }
-<<<<<<< HEAD
+
 };
-=======
+
 };
->>>>>>> 439d731f31ec0cfc365a3cdf5195d641754da135
+
